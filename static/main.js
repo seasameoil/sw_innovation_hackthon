57bc5d5434234fc6dcd2ff737c141699f9a4e3ee
seasameoil <whdwlgus12@gmail.com>
2023-11-02 02:10:29 +0900

    html

--- static/main.js
+++ static/main.js
@@ -1,10 +1,28 @@
 let socket = new WebSocket('ws://localhost:8000/ws/graph/');
 
 socket.onmessage = function(e) {
+    var pageUrl = window.location.href;
+    var last = pageUrl.substring(pageUrl.length-4);
+
     let djangoData = JSON.parse(e.data);
     console.log(djangoData);
 
-    document.querySelector('#hour').innerHTML = djangoData.hour;
+    if (last == "main") {
 
-    
+        if (djangoData.fire == 'True') {
+             document.getElementById('btn1').className = 'bus blink';
+             document.getElementById('btn2').className = 'bus';
+             document.getElementById('btn3').className = 'bus';
+        // } else if (djangoData.count == true) {
+            // document.getElementById('btn1').className = 'bus';
+            // document.getElementById('btn2').className = 'bus blink';
+            // document.getElementById('btn3').className = 'bus';
+        } else if (djangoData.crime == 'ON') {
+            document.getElementById('btn1').className = 'bus';
+            document.getElementById('btn2').className = 'bus';
+            document.getElementById('btn3').className = 'bus blink';
+        }
+
+        document.getElementById('count').innerHTML = djangoData.count;
+    }
 }
